fd9b9ea76d2e24f97a75a20ddef013409a460775
hayoung78 <wendy4332@naver.com>
2024-01-22 21:43:36 +0900

    feat: eslint, prettier 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			modules: true,
+			jsx: true,
+		},
+		ecmaVersion: 2020,
+		sourceType: "module",
+	},
+	plugins: ["react", "unused-imports", "simple-import-sort"],
+	rules: {
+		"react/jsx-uses-vars": "error",
+		"react/jsx-uses-react": "error",
+		"space-before-function-paren": [
+			"error",
+			{
+				anonymous: "ignore",
+				named: "never",
+				asyncArrow: "always",
+			},
+		],
+		"unused-imports/no-unused-imports-ts": ["error"],
+		"simple-import-sort/imports": "error",
+	},
+};
