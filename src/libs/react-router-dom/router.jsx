b0e5d8740dc96de93979f6dc7600ab9eb86d13bf
jinsol <skek298@naver.com>
2024-01-23 22:07:42 +0900

    fix: barrel file 수정 및 설정 완료

--- src/libs/react-router-dom/router.jsx
+++ src/libs/react-router-dom/router.jsx
@@ -1,3 +1,6 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { RootLayout } from "../../components/index";
 import {
 	DetailProductPage,
 	MyInfoPage,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
-export { default as DetailProductPage } from "./products/detail-product-page";
-export { default as MyInfoPage } from "./my-info-page";
-export { default as ProductListPage } from "./product-list-page";
-export { default as RegisterProductPage } from "./products/register-product-page";
-export { default as SigninPage } from "./sign/signin-page";
-export { default as SignupPage } from "./sign/signup-page";
-export { default as UsedProductPage } from "./products/used-product-page";
+import MyInfoPage from "./my-info-page";
+import ProductListPage from "./product-list-page";
+import DetailProductPage from "./products/detail-product-page";
+import RegisterProductPage from "./products/register-product-page";
+import UsedProductPage from "./products/used-product-page";
+import SigninPage from "./sign/signin-page";
+import SignupPage from "./sign/signup-page";
+
+export {
+	DetailProductPage,
+	MyInfoPage,
+	ProductListPage,
+	RegisterProductPage,
+	SigninPage,
+	SignupPage,
+	UsedProductPage,
+};
