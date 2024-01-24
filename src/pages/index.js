4d736d6a82dc4eaac2345d27c73612aeba7dd791
jinsol <skek298@naver.com>
2024-01-24 16:25:47 +0900

    chore: 폴더 위치 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import MyInfoPage from "./my-info-page";
-import ProductListPage from "./product-list-page";
 import DetailProductPage from "./products/detail-product-page";
+import ProductListPage from "./products/product-list-page";
 import RegisterProductPage from "./products/register-product-page";
 import UsedProductPage from "./products/used-product-page";
 import SigninPage from "./sign/signin-page";
