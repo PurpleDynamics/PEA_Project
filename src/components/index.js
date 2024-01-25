d4ee3d2e0df9066e33253c3187f9fe8a94daa24d
snkchan <144839872+snkchan@users.noreply.github.com>
2024-01-26 03:03:46 +0900

    design: Footer Componet작업했습니다.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import Footer from "./layouts/footer";
+import Header from "./layouts/header/header";
+import HeaderIcons from "./layouts/header/header-icons";
 import RootLayout from "./layouts/layout";
-
-export { RootLayout };
+import SearchBar from "./search-bar";
+export { RootLayout, Header, Footer, SearchBar, HeaderIcons };
---
+++ src/components/layouts/footer.jsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+import {
+	COLOR,
+	FONT_SIZE,
+} from "../../libs/styled-components/reference-tokens";
+const Footer = () => {
+	return (
+		<S.MainContainer>
+			<S.FooterWrapper>
+				<S.LeftTextBox>
+					<S.Text>
+						사업자명: Permanent Energy-saving Affect | 주소:
+						서울특별시 강남구 테헤란로 146
+					</S.Text>
+					<S.Text>
+						대표 : PurpleDynamics | 사업자등록번호 : 000-00-00000
+					</S.Text>
+				</S.LeftTextBox>
+				<S.RightTextBox>
+					<S.Number>고객센터 1111-1111</S.Number>
+					<S.Text>월~금 09:00~18:00</S.Text>
+				</S.RightTextBox>
+			</S.FooterWrapper>
+		</S.MainContainer>
+	);
+};
+export default Footer;
+const MainContainer = styled.div`
+	width: 100%;
+	height: 13rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: ${COLOR.COMMON[1000]};
+	border-top: 1px solid ${COLOR.COMMON[0]};
+`;
+const FooterWrapper = styled.div`
+	width: 125rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-gap: 20rem;
+`;
+
+const LeftTextBox = styled.div`
+	width: 70rem;
+	display: flex;
+	flex-direction: column;
+`;
+
+const RightTextBox = styled.div`
+	width: 30rem;
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-end;
+	flex-direction: column;
+`;
+const Number = styled.p`
+	font-size: ${FONT_SIZE.md};
+	padding: 1rem 0;
+`;
+const Text = styled.p`
+	padding: 1rem 0;
+	font-size: ${FONT_SIZE.sm};
+	font-family: "SOYO_Maple_Regular";
+`;
+
+const S = {
+	MainContainer,
+	FooterWrapper,
+	Number,
+	Text,
+	LeftTextBox,
+	RightTextBox,
+};
