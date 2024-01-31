10024aa8da61037af3674ac6b28da44ab7b0e7da
snkchan <144839872+snkchan@users.noreply.github.com>
2024-01-31 11:21:12 +0900

    chore: banner 컴포넌트 생성

--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,6 +1,7 @@
+import Banner from "./banner";
 import Button from "./button";
 import CategoryToggle from "./category-toggle";
 import Input from "./input";
 import ResponsiveIcon from "./responsive-icon";
 import SearchBar from "./search-bar";
-export { Button, CategoryToggle, Input, ResponsiveIcon, SearchBar };
+export { Banner, Button, CategoryToggle, Input, ResponsiveIcon, SearchBar };
