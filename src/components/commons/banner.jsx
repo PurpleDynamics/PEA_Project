988ebf4cb77a77caee04d68e97b7ba823280c372
snkchan <144839872+snkchan@users.noreply.github.com>
2024-02-02 05:01:17 +0900

    feat: <,>아이콘 클릭시 배너이동 기능 추가, anmation효과 적용

--- src/components/commons/banner.jsx
+++ src/components/commons/banner.jsx
@@ -1,120 +1,166 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import { COLOR, FONT_SIZE } from "../../libs/styled-components";
 import ResponsiveIcon from "./responsive-icon";
+/**
+ * @component
+ * @returns {JSX.Element}
+ * @description
+ * - "https://purpledynamics.github.io/ads/"
+ * - 깃허브 사이트에 넘어온 광고정보를 바탕으로
+ * - 배너창에 광고정보를 보여주는 컴포넌트입니다.
+ */
+
 const Banner = () => {
-	const [data, setData] = useState([]);
-	const [curIdx, setCurIdx] = useState(null);
-	const fetchData = async () => {
-		const response = await axios.get("http://purpledynamics.github.io/ads");
-		console.log(response.data);
-		setData(response.data);
+	const [adData, setAdData] = useState([]); // fetching된 data를담을 state
+	const [curIdx, setCurIdx] = useState(null); // 배열로넘어온 data의 idx를담을 state
+
+	// github page로부터 광고데이터 fetching 진행
+	const fetchAdData = async () => {
+		const response = await axios.get(
+			"https://purpledynamics.github.io/ads/"
+		);
+		setAdData(response.data);
 		setCurIdx(0);
 	};
+
+	// adData or curIdx의 값이변하면 다시 fetching진행
 	useEffect(() => {
-		fetchData();
-	}, [setData, setCurIdx]);
-	
+		fetchAdData();
+	}, [setAdData, setCurIdx]);
+
+	// banner click event
+	const onClickBanner = () => {
+		window.open(adData[curIdx]?.redirectUrl); //해당사이트로 이동
+	};
+
+	//배너의 '<' 아이콘 click event
+	const onClickLeftIcon = () => {
+		setCurIdx((prev) => {
+			if (prev <= 0) {
+				return adData.length - 1;
+			}
+			return prev - 1;
+		});
+	};
+	//배너의 '>' 아이콘 click event
+	const onClickRightIcon = () => {
+		setCurIdx((prev) => {
+			if (prev >= adData.length - 1) {
+				return 0;
+			}
+			return prev + 1;
+		});
+	};
+
 	return (
-		<S.CenterContainer $bgColor={data[curIdx]?.backgroundColor}>
-			<S.MainWrapper>
-				<S.InfoContainer>
-					<S.InfoWrapper>
-						<S.CompanyName>
-							{data[curIdx]?.companyName}
-						</S.CompanyName>
-						<S.InfoTitle $themeColor={data[curIdx]?.titleColor}>
-							{data[curIdx]?.titleText}
-						</S.InfoTitle>
-						<S.InfoText $contentColor={data[curIdx]?.contentColor}>
-							{data[curIdx]?.contentText
-								.split("  ")
-								.map((phrase, idx) => {
-									return (
-										<div key={idx}>
-											{phrase}
-											<br />
-										</div>
-									);
-								})}
-						</S.InfoText>
-						<S.InfoSlideBtn>
-							<S.InfoSlideBtnWrapper>
-								<ResponsiveIcon
-									color={COLOR.COMMON[1000]}
-									size={FONT_SIZE.bg}
-									icon={BsChevronLeft}
-									onClick={() => {
-										setCurIdx((prev) => {
-											if (prev <= 0) {
-												return prev;
-											}
-											return prev - 1;
-										});
-									}}
-								/>
-								{curIdx + 1}/{data.length}
-								<ResponsiveIcon
-									color={COLOR.COMMON[1000]}
-									size={FONT_SIZE.bg}
-									icon={BsChevronRight}
-									onClick={() => {
-										setCurIdx((prev) => {
-											if (prev >= data.length - 1) {
-												return prev;
-											}
-											return prev + 1;
-										});
-									}}
-								/>
-							</S.InfoSlideBtnWrapper>
-						</S.InfoSlideBtn>
-					</S.InfoWrapper>
-				</S.InfoContainer>
-				<S.ImageContainer>
-					<S.BannerImage src={data[curIdx]?.imgUrl} />
-					{/**외부에서 이미지 경로 받아서 사용 */}
-				</S.ImageContainer>
-			</S.MainWrapper>
+		<S.CenterContainer
+			key={curIdx}
+			$bgColor={adData[curIdx]?.backgroundColor}
+			onClick={onClickBanner}
+		>
+			<S.InfoWrapper>
+				<S.CompanyName>{adData[curIdx]?.companyName}</S.CompanyName>
+				<S.InfoTitle $titleColor={adData[curIdx]?.titleColor}>
+					{adData[curIdx]?.titleText}
+				</S.InfoTitle>
+				<S.InfoText $contentColor={adData[curIdx]?.contentColor}>
+					{adData[curIdx]?.contentText
+						.split("  ")
+						.map((phrase, idx) => {
+							return (
+								<div key={idx}>
+									{phrase}
+									<br />
+								</div>
+							);
+						})}
+				</S.InfoText>
+				<S.InfoSlideBtn>
+					<S.InfoSlideBtnWrapper
+						onClick={(e) => {
+							e.stopPropagation();
+						}}
+					>
+						<ResponsiveIcon
+							color={COLOR.COMMON[1000]}
+							size={FONT_SIZE.bg}
+							icon={BsChevronLeft}
+							onClick={onClickLeftIcon}
+						/>
+						{curIdx + 1}/{adData.length}
+						<ResponsiveIcon
+							color={COLOR.COMMON[1000]}
+							size={FONT_SIZE.bg}
+							icon={BsChevronRight}
+							onClick={onClickRightIcon}
+						/>
+					</S.InfoSlideBtnWrapper>
+				</S.InfoSlideBtn>
+			</S.InfoWrapper>
+			<S.ImageContainer>
+				<S.BannerImage src={adData[curIdx]?.imgUrl} />
+			</S.ImageContainer>
 		</S.CenterContainer>
 	);
 };
 
 export default Banner;
 
+// opacity_control, rising animation효과
+const opacity_control = keyframes`
+	0%{
+		background-color: ${COLOR.COMMON[0]};
+		opacity: 0.8
+	}
+	100%{
+		opacity: 1
+	}
+`;
+
+const rising = keyframes`
+0%{
+	opacity: 0;
+	transform : translateY(5%);
+}
+100%{
+	opacity: 1;
+	transform: translateY(0%);
+}
+`;
 const CenterContainer = styled.div`
 	margin: 9rem 0;
 	width: 100%;
-	height: 35rem;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	height: fit-content;
+	animation: ${opacity_control} 0.5s ease-in-out forwards;
 	background-color: ${({ $bgColor }) => {
 		return $bgColor;
 	}};
-`;
-const MainWrapper = styled.div`
-	display: grid;
-	grid-template-columns: 60% 40%;
-	width: 110rem;
-	height: 100%;
-`;
-const InfoContainer = styled.div`
-	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	&:hover {
+		cursor: pointer;
+	}
 `;
 const InfoWrapper = styled.div`
+	height: 100%;
+	width: 50rem;
+
 	padding-top: 5rem;
 	display: grid;
 	grid-template-rows: 1fr 2fr 6fr 3fr;
-	justify-content: center;
+	justify-content: start;
 	align-items: center;
 `;
 const CompanyName = styled.h3`
 	grid-row: 1;
+	opacity: 0;
 	color: ${COLOR.COMMON[1000]};
+	animation: ${rising} 1s ease-in-out forwards;
 `;
 const InfoSlideBtn = styled.button`
 	grid-row: 4;
@@ -132,19 +178,23 @@ const InfoSlideBtnWrapper = styled.div`
 `;
 const InfoTitle = styled.h1`
 	grid-row: 2;
-	color: ${({ $themeColor }) => {
-		return $themeColor;
+	color: ${({ $titleColor }) => {
+		return $titleColor;
 	}};
+	opacity: 0;
+	animation: ${rising} 1s 0.2s ease-in-out forwards;
 `;
 const InfoText = styled.h3`
 	grid-row: 3;
 	color: ${({ $contentColor }) => {
 		return $contentColor;
 	}};
+	opacity: 0;
 	font-family: "SOYO_Maple_Regular";
 	line-height: ${FONT_SIZE.lg};
+	animation: ${rising} 1s 0.4s ease-in-out forwards;
+	word-break: break-all;
 `;
-
 const ImageContainer = styled.div`
 	display: flex;
 	justify-content: center;
@@ -155,12 +205,12 @@ const ImageContainer = styled.div`
 const BannerImage = styled.img`
 	width: 100%;
 	height: 100%;
+	opacity: 0;
+	animation: ${rising} 1s 0.6s ease-in-out forwards;
 `;
-// 배너 제목에 대한 색상 조합
+
 const S = {
 	CenterContainer,
-	MainWrapper,
-	InfoContainer,
 	InfoWrapper,
 	CompanyName,
 	InfoSlideBtn,
