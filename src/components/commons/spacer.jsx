1a7af08bbdcae7e140ba286c8f3d0466e5bf00bd
jjjiyoung0130 <jiyoung63@nate.com>
2024-02-05 01:44:21 +0900

    refactor: 스타일드 컴포넌트로 변경 및 props 수정

--- src/components/commons/spacer.jsx
+++ src/components/commons/spacer.jsx
@@ -1,35 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 /**
  *
  * @component
- * @parameter size : number - 크기 값 지정
- * @parameter axis : string - spacer의 축(방향) 지정 ["vertical" 또는 "horizontal"]
- * @parameter style = {} : object : 추가적인 스타일 객체
+ * @parameter width : number - 가로 너비 지정 (단위: rem)
+ * @parameter height : number - 세로 높이 지정 (단위: rem)
+ * @parameter rest : any
  * @returns {JSX.Element}
  *
- * @description
- * - props로 전달받은 size와 axis에 따라 빈 공간을 생성하는 컴포넌트 입니다.
- * - axis를 전달하지 않을 경우, size * size (단위: px)의 정사각형을 생성합니다.
- * - axis가 "vertical"인 경우, props로 전달받은 size 만큼 세로 높이의 빈 공간을 생성합니다. (1px * size(px))
- * - axis가 "horizontal"인 경우, props로 전달받은 size 만큼 가로 너비의 빈 공간을 생성합니다. (size(px) * 1px)
+ * @description - props로 전달받은 width, height에 따라 빈 공간을 생성하는 컴포넌트 입니다.
  */
 
-const Spacer = ({ size, axis, style = {}, ...rest }) => {
-	const width = axis === "vertical" ? 1 : size;
-	const height = axis === "horizontal" ? 1 : size;
-	return (
-		<span
-			style={{
-				display: "block",
-				width,
-				minWidth: width,
-				height,
-				minHeight: height,
-				...style,
-			}}
-			{...rest}
-		/>
-	);
+const Spacer = ({ width, height, ...rest }) => {
+	return <S.SpacerContainer width={width} height={height} {...rest} />;
 };
 export default Spacer;
+
+const SpacerContainer = styled.span`
+	display: block;
+	width: ${({ width }) => width}rem;
+	height: ${({ height }) => height}rem;
+	min-width: ${({ width }) => width}rem;
+	min-height: ${({ height }) => height}rem;
+`;
+
+const S = {
+	SpacerContainer,
+};
