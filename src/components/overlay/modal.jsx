89204ce9649e3c7e681361984b698aa0b089a4c2
keeprok <dldudfhr425@gmail.com>
2024-02-06 00:49:58 +0900

    fix : 버튼사이즈 조절및 radius조절

--- src/components/overlay/modal.jsx
+++ src/components/overlay/modal.jsx
@@ -8,6 +8,7 @@ import { COLOR, FONT_SIZE } from "../../libs/styled-components";
  * @parameter onClickBaseBtn : function - 기본 버튼의 기능
  * @parameter addText : string - 추가되는 버튼 TEXT
  * @parameter onClickAddBtn : function - 추가되는 버튼의 기능
+ * @parameter onClose : function 모달을 끄는 기능
  * @returns {JSX.Element}
  *
  * @description
@@ -16,6 +17,7 @@ import { COLOR, FONT_SIZE } from "../../libs/styled-components";
  * 기본적으로 하나의 버튼만 보여집니다
  * "addText"이 추가 될시 화면상 왼쪽부분에 추가됩니다
  * "modalWidth"전달되지않을 경우 default값 30rem 사이즈가 출력됩니다
+ * overlay 를 통해 modal사용시 baseBtn에서
  * @example
  * - <Modal modalText="사용 가능한 이메일 입니다 modalContents={ModalContents} "$modalWidth="40rem" baseBtn={BaseBtn} addBtn={AddBtn} addText="취소" />
  */
@@ -56,7 +58,6 @@ const Wrapper = styled.div`
 		return width;
 	}};
 	height: fit-content;
-	min-height: 14rem;
 	border-radius: 8px;
 	border: 0.1rem solid ${COLOR.COMMON[600]};
 	display: flex;
@@ -72,12 +73,14 @@ const Wrapper = styled.div`
 const ModalContentBox = styled.div`
 	width: 100%;
 	height: 100%;
+	min-height: 7rem;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 `;
 const BtnBox = styled.div`
 	width: 100%;
+	min-height: 7rem;
 	height: 100%;
 	border-top: 1px solid ${COLOR.COMMON[600]};
 	display: flex;
@@ -94,10 +97,12 @@ const buttonStyle = css`
 const BaseBtn = styled.button`
 	${buttonStyle}
 	color: ${COLOR.SYSTEM.success};
+	border-radius: ${({ addText }) => (addText ? "0 8px 8px 0" : "8px")};
 `;
 const AddBtn = styled.button`
 	${buttonStyle}
 	color: ${COLOR.COMMON[400]};
+	border-radius: 8px 0 0 8px;
 `;
 
 const S = {
