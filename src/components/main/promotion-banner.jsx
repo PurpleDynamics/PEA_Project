3e7b9c23f52a0ec4a1f112b3505c81c430d89bbb
jinsol <skek298@naver.com>
2024-02-06 23:47:52 +0900

    refactor: container와 wrapper의 위치를 변경했습니다. fix: promotion banner 가 중간에 오도록 조정했습니다.

--- src/components/main/promotion-banner.jsx
+++ src/components/main/promotion-banner.jsx
@@ -12,48 +12,66 @@ import { Button, CompressionContainer } from "../commons";
 
 const PromotionBanner = () => {
 	return (
-		<S.PromoWrapper>
+		<S.PromoContainer>
 			<CompressionContainer tb="12rem">
-				<S.PromoContainer>
-					<S.PromoImg src="https://url.kr/wmz9uc" />
-					<S.PromoRightBox>
-						<S.PromoTitle>
-							"너, 참 <S.PromoTitleSpan>중고</S.PromoTitleSpan>
-							스럽다!"
-						</S.PromoTitle>
-						<S.PromoContext>
-							<S.PromoContextSpan>P.E.A</S.PromoContextSpan>
-							에서 <br />
-							지금 당장 거래하세요.!
-						</S.PromoContext>
-						<S.ButtonBox>
-							<Button palette="orange" width="13rem">
-								중고거래
-							</Button>
-							<Button palette="mint" width="13rem">
-								무료나눔
-							</Button>
-						</S.ButtonBox>
-					</S.PromoRightBox>
-				</S.PromoContainer>
+				<S.PromoCenterBox>
+					<S.PromoWrapper>
+						<S.PromoImg src="https://url.kr/wmz9uc" />
+						<S.PromoRightBox>
+							<S.PromoTitle>
+								"너, 참{" "}
+								<S.PromoTitleSpan>중고</S.PromoTitleSpan>
+								스럽다!"
+							</S.PromoTitle>
+							<S.PromoContext>
+								<S.PromoContextSpan>P.E.A</S.PromoContextSpan>
+								에서 <br />
+								지금 당장 거래하세요.!
+							</S.PromoContext>
+							<S.ButtonBox>
+								<Button palette="orange" width="13rem">
+									중고거래
+								</Button>
+								<Button palette="mint" width="13rem">
+									무료나눔
+								</Button>
+							</S.ButtonBox>
+						</S.PromoRightBox>
+					</S.PromoWrapper>
+				</S.PromoCenterBox>
 			</CompressionContainer>
-		</S.PromoWrapper>
+		</S.PromoContainer>
 	);
 };
 export default PromotionBanner;
 
 const PromoWrapper = styled.div`
-	width: 100%;
-	height: 60rem;
-	background-color: ${COLOR.PALETTE.yellow.light};
+	width: 100rem;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	align-items: center;
+	justify-content: center;
+	gap: 2rem;
+	@media (max-width: ${BREAK_POINT.lg}) {
+		grid-template-columns: 1fr;
+		justify-content: center;
+	}
 `;
 
 const PromoContainer = styled.div`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	@media (max-width: ${BREAK_POINT.lg}) {
-		display: block;
-	}
+	width: 100%;
+	height: 60rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: ${COLOR.PALETTE.yellow.light};
+`;
+
+const PromoCenterBox = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 const PromoImg = styled.img`
@@ -108,6 +126,7 @@ const ButtonBox = styled.div`
 const S = {
 	PromoWrapper,
 	PromoContainer,
+	PromoCenterBox,
 	PromoImg,
 	PromoRightBox,
 	PromoTitle,
