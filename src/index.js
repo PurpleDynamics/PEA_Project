9b37d17fc276cf479eba2f00acb6ee3c33ce8b9c
yunshin <2younsin@gmail.com>
2024-02-06 17:33:14 +0900

    config: msw 의존성 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
 import { OverlayProvider } from "./contexts";
+import { worker } from "./libs/msw/browser";
 import { router } from "./libs/react-router-dom";
 import GlobalStyles from "./libs/styled-components";
+
+if (process.env.NODE_ENV === "development") {
+	worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
