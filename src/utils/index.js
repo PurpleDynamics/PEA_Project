3ae23b77a693048df079dcdac11f4959d67d427d
hayoung78 <wendy4332@naver.com>
2024-02-08 00:46:20 +0900

    fix: auth파일 이름 수정 및 docs 내용 수정, token key값 수정

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 export * from "./auto-color-manager";
 export * from "./data-calculator";
-export * from "./auth";
+export * from "./storage-manager";
 export * from "./instance";
---
+++ src/utils/storage-manager.js
@@ -0,0 +1,45 @@
+/**
+ * localStorage를 사용하여 애플리케이션 인증 토큰을 관리하는 파일입니다.
+ */
+
+/**
+ * @function
+ * @retruns {string | null} 저장된 token값 또는 token이 없을 경우 null
+ *
+ * @description localStorage에 저장된 내용을 key값("accessToken")으로 가져오는 함수입니다.
+ */
+export const getLocalToken = () => {
+	return localStorage.getItem("accessToken");
+};
+
+/**
+ * @function
+ * @parameter token: string - 로컬스토리지에 저장될 토큰
+ *
+ * @description localStorage에 key값("accessToken")으로 받아온 token을 저장하는 함수입니다.
+ */
+export const setLocalToken = (token) => {
+	return localStorage.setItem("accessToken", token);
+};
+
+/**
+ * @function
+ * @parameter token : 세션스토리지에 저장될 토큰
+ *
+ * @description sessionStorage에 key값("refreshToken")으로 받아온 token을 저장하는 함수입니다.
+ */
+
+export const setSessionToken = (token) => {
+	return sessionStorage.setItem("refreshToken", token);
+};
+
+/**
+ * @function
+ * @retruns {string | null} 저장된 token값 또는 token이 없을 경우 null
+ *
+ * @description sessionStorage에 key값("refreshToken")으로 받아온 token을 저장하는 함수입니다.
+ */
+
+export const getSessionToken = () => {
+	return sessionStorage.getItem("refreshToken");
+};
