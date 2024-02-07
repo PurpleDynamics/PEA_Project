43034887b9c5c3b6f6ef9fda1f2236f5165b939b
hayoung78 <wendy4332@naver.com>
2024-02-07 15:59:42 +0900

    feat : 토큰 관리를위한 auth파일 생성

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,14 @@
 /**
  * @functions
  * @parameter token : 로컬스토리지에 저장될 토큰
- * @description - 로컬/세션 스토리지에서 토큰을 관리하는데 사용되는 함수입니다.
+ * @description - 로컬스토리지에서 토큰을 관리하는데 사용되는 함수입니다.
  *
  */
 
-export const getLocalToken = () => {
-	return localStorage.getItem("token");
+export const getToken = () => {
+	localStorage.getItem("token");
 };
 
-export const setLocalToken = (token) => {
-	return localStorage.setItem("token", token);
-};
-
-export const setSessionToken = (token) => {
-	return sessionStorage.setItem("token", token);
-};
-export const getSessionToken = () => {
-	return sessionStorage.getItem("token");
+export const setToken = (token) => {
+	localStorage.setItem("token", token);
 };
