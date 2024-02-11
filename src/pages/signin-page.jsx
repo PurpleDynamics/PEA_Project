a5f64080e045a8e6182332a4a929628c1d2bbdc5
hayoung78 <wendy4332@naver.com>
2024-02-11 17:42:57 +0900

    fix: 자동로그인 체크시 로직 설명 추가

--- src/pages/signin-page.jsx
+++ src/pages/signin-page.jsx
@@ -45,8 +45,8 @@ const SigninPage = () => {
 		try {
 			const response = await axios.post(`${BASE_URL}/signin`, Data);
 			const token = response.data.token;
-			setToken("token", token);
-			// 로그인이 성공하면 mainPage로 이동
+
+			// 로그인이 성공적이고, 자동로그인 체크시 localStorage에 , 미체크시 sessionStorage에 토큰이 저장
 			if (response.status === 200) {
 				if (autoLogin) {
 					setLocalToken("accessToken", token);
