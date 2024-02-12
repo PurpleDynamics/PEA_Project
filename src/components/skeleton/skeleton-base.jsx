09259770d73938494f6c3a30d0f9d761ee4cb49b
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-02-13 02:23:36 +0900

    design: skeleton ui를 만들었습니다.

---
+++ src/components/skeleton/skeleton-base.jsx
@@ -0,0 +1,69 @@
+import styled, { keyframes } from "styled-components";
+
+import { COLOR } from "../../libs/styled-components";
+
+/**
+ * @component
+ * @parameter $width : string - 넓이를 입력 받습니다.
+ * @parameter $height : string - 높이를 입력 받습니다.
+ * @parameter $borderRadius : string - border-radius값을 입력 받습니다.
+ * @returns {JSX.Element}
+ *
+ * @description
+ * - skeleton ui component 입니다.
+ * - ex)
+ * - <div>
+ * - 	<SkeletonBase $width="1rem" $height="1rem" $borderRadius="1rem" />
+ * - </div>
+ * - 이런식으로 사용하시면 됩니다.
+ */
+
+const SkeletonBase = ({ $width, $height, $borderRadius }) => {
+	return (
+		<S.Skeleton
+			width={$width}
+			height={$height}
+			borderRadius={$borderRadius}
+		/>
+	);
+};
+export default SkeletonBase;
+
+const loadingAnimation = keyframes`
+    0% {
+        transform: translateX(-100%)
+    }
+    100%{
+        transform: translateX(100%)
+    }
+`;
+
+const Skeleton = styled.div`
+	background-color: ${COLOR.COMMON[800]};
+	border-radius: ${({ borderRadius }) => borderRadius};
+	overflow: hidden;
+	position: relative;
+	width: ${({ width }) => width};
+	height: ${({ height }) => height};
+
+	&::after {
+		content: "";
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		width: 100%;
+		background: linear-gradient(
+			90deg,
+			transparent,
+			${COLOR.COMMON[1000]},
+			transparent
+		);
+		animation: ${loadingAnimation} 1.5s infinite linear;
+	}
+`;
+
+const S = {
+	Skeleton,
+};
---
+++ src/components/skeleton/skeleton-product-card.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+import { COLOR } from "../../libs/styled-components";
+import SkeletonBase from "./skeleton-base";
+
+/**
+ * @component
+ * @returns {JSX.Element}
+ *
+ * @description product card의 skeleton ui입니다.
+ */
+
+const SkeletonProductCard = () => {
+	return (
+		<S.SkeletonWrapper>
+			<SkeletonBase $width="21rem" $height="21rem" />
+			<SkeletonBase $width="21rem" $height="3rem" />
+			<SkeletonBase $width="21rem" $height="3rem" />
+			<SkeletonBase $width="21rem" $height="3rem" />
+		</S.SkeletonWrapper>
+	);
+};
+export default SkeletonProductCard;
+
+const SkeletonWrapper = styled.div`
+	width: 21rem;
+	height: 35.8rem;
+	display: grid;
+	grid-template-rows: 21.5rem 1fr 1.2fr 1fr;
+	grid-row-gap: 1px;
+	border: 0.1rem solid ${COLOR.COMMON[800]};
+	border-radius: 1.1rem;
+	overflow: hidden;
+`;
+
+const S = {
+	SkeletonWrapper,
+};
