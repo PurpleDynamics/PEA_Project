fc7c8fd38a8f7180852b6c5565b538c31fb46906
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-02-12 18:08:41 +0900

    design: chatting-overlay-common 생성

---
+++ src/components/overlay/chatting/chatting-overlay.jsx
@@ -0,0 +1,130 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+import { COLOR, FONT_SIZE } from "../../../libs/styled-components";
+import { SearchBar } from "../../commons";
+import ChatRoomList from "./chat-room-list";
+
+/**
+ * @component
+ * @parameter onClose: function - x 를 눌렀을 때 overlay를 닫아주는 함수입니다.
+ * @returns {JSX.Element}
+ *
+ * @description 채팅을 클릭했을 때 제일 먼저 나오는 페이지 입니다.
+ */
+
+const ChattingOverlay = ({ onClose }) => {
+	const [chatRoomList, setChatRoomList] = useState([
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+		{
+			nickname: "김진솔",
+			createdAt: "20:26",
+			lastMessage: "아 너무 졸립고~",
+		},
+	]);
+
+	return (
+		<S.ChatWrapper>
+			<S.ChatTitleBox>
+				<S.ChatTitleImage src="https://url.kr/fm7ls1" />
+				<S.ChatTitleWrapper>
+					<S.ChatTitleText>P.E.A Chat</S.ChatTitleText>
+					<S.X_Button onClick={onClose}>x</S.X_Button>
+				</S.ChatTitleWrapper>
+			</S.ChatTitleBox>
+
+			<S.SearchContainer>
+				<SearchBar
+					width="30rem"
+					paddingLeft="2.5rem"
+					placeholder="판매자명 또는 상품명을 입력해주세요"
+				/>
+			</S.SearchContainer>
+			<ChatRoomList data={chatRoomList} />
+		</S.ChatWrapper>
+	);
+};
+export default ChattingOverlay;
+
+const ChatWrapper = styled.div`
+	width: 35rem;
+	height: 40rem;
+	border-radius: 0.6rem;
+	box-shadow: 0.1rem 0.2rem 0.4rem 0.1rem ${COLOR.COMMON[400]};
+
+	background-color: ${COLOR.COMMON[1000]};
+`;
+
+const ChatTitleBox = styled.div`
+	display: flex;
+	justify-content: left;
+	align-items: center;
+`;
+
+const ChatTitleWrapper = styled.div`
+	width: 30rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const ChatTitleImage = styled.img`
+	width: 4rem;
+	padding: 0.5rem;
+`;
+
+const ChatTitleText = styled.p`
+	font-size: ${FONT_SIZE.md};
+`;
+
+const SearchContainer = styled.div`
+	padding: 1rem 0;
+`;
+
+const X_Button = styled.button`
+	width: 3rem;
+	height: 3rem;
+	background-color: ${COLOR.COMMON[1000]};
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const S = {
+	ChatWrapper,
+	X_Button,
+	ChatTitleBox,
+	ChatTitleImage,
+	ChatTitleText,
+	ChatTitleWrapper,
+	SearchContainer,
+};
