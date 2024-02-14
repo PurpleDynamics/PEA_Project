c8c3d94bee6ee07f9082f88d7f478c42fcee5364
hayoung78 <wendy4332@naver.com>
2024-02-14 17:22:21 +0900

    fix : 유효성 검사 정규 표현식 수정

--- src/constants/validate-message.js
+++ src/constants/validate-message.js
@@ -16,14 +16,22 @@ const PASSWORD = {
 	value: /^(.{8,})$/,
 	message: "8글자이상 입력해주세요",
 };
+
 const PASSWORD_CONFIRM = {
 	message: "비밀번호가 일치하지 않습니다",
 };
+
 const NICKNAME = {
-	value: /^[a-zA-Z0-9]*$/,
+	value: /^[a-zA-Z0-9가-힣]{1,8}$/,
 	message: "특수문자는 사용할 수 없습니다",
 };
+
 const COMMON_MESSAGE = "필수 응답 항목입니다.";
+
+const PHONE_NUMBER = {
+	value: /^[0-9]+$/,
+	message: "숫자만 입력가능합니다",
+};
 
 export const VAILDATION = {
 	EMAIL,
@@ -31,4 +39,5 @@ export const VAILDATION = {
 	PASSWORD_CONFIRM,
 	NICKNAME,
 	COMMON_MESSAGE,
+	PHONE_NUMBER,
 };
