2c61ff4d5e3acad6f5242c0742db8f314f6d631a
hayoung78 <wendy4332@naver.com>
2024-02-14 11:42:55 +0900

    feat: used-product시작

--- src/pages/used-product-page.jsx
+++ src/pages/used-product-page.jsx
@@ -1,8 +1,26 @@
+import styled from "styled-components";
+
+import { Banner, Spacer } from "../components/commons";
+
 const UsedProductPage = () => {
 	return (
 		<>
-			<div>UsedProductPage</div>
+			<S.Wrapper>
+				<Spacer height="2rem" />
+				<Banner />
+				{/* <EightProductGrid
+					userData={user}
+					eightProductData={usedData}
+					salesCategory={"중고거래"}
+				/> */}
+			</S.Wrapper>
 		</>
 	);
 };
 export default UsedProductPage;
+
+const Wrapper = styled.div``;
+
+const S = {
+	Wrapper,
+};
