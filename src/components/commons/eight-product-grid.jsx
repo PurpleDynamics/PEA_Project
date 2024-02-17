d2d99cf28d18f9d427c77bc50288ea553041619c
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-02-18 02:41:26 +0900

    fix/eight-product-grid

---
+++ src/components/commons/eight-product-grid.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { BREAK_POINT, COLOR, FONT_SIZE } from "../../libs/styled-components";
+import { ProductCard } from ".";
+
+/**
+ * @component
+ * @parameter productData : array<상품데이터> - product data를 받아옵니다.
+ * @parameter onInterestButtonClick : function - 관심 버튼 클릭 시 일어나는 이벤트 함수
+ * @returns {JSX.Element}
+ *
+ * @description 상품데이터를 받아와 8개의 product card를 보여주는 component입니다.
+ */
+
+const EightProductGrid = ({ productData, onInterestButtonClick }) => {
+	const [eightProductData, setEightProductData] = useState([]);
+
+	const dataSlice = productData.slice(0, 8);
+
+	useEffect(() => {
+		setEightProductData(dataSlice);
+	}, [productData]);
+
+	return (
+		<S.Wrapper>
+			{eightProductData.length > 0 ? (
+				<S.ProductList>
+					{eightProductData.map((data, index) => (
+						<ProductCard
+							key={index}
+							productId={data.productId}
+							imgUrl={data.imgUrl}
+							title={data.title}
+							price={data.price}
+							createdAt={data.createdAt}
+							categoriesArray={data.categoriesArray}
+							interestCount={data.interestCount}
+							chattingCount={data.chattingCount}
+							initIsInterest={data.initIsInterest}
+							onClickInterestButton={() =>
+								onInterestButtonClick(data.productId)
+							}
+							disabled={data.disabled}
+						/>
+					))}
+				</S.ProductList>
+			) : (
+				<S.NoDataText>등록된 상품이 없습니다.</S.NoDataText>
+			)}
+		</S.Wrapper>
+	);
+};
+
+export default EightProductGrid;
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 2rem 0;
+`;
+
+const TextWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	padding-bottom: 4rem;
+`;
+
+const LocationText = styled.p`
+	font-size: ${FONT_SIZE.bg};
+`;
+
+const UsedFreeText = styled.span`
+	font-size: ${FONT_SIZE.bg};
+	color: ${COLOR.SUB[600]};
+`;
+
+const ProductList = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 7.5rem;
+	@media (max-width: ${BREAK_POINT.lg}) {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
+	@media (max-width: 700px) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+	}
+`;
+
+const NoDataText = styled.h1`
+	padding: 25rem;
+`;
+
+const S = {
+	Wrapper,
+	TextWrapper,
+	LocationText,
+	UsedFreeText,
+	NoDataText,
+	ProductList,
+};
--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -2,6 +2,7 @@ import Banner from "./banner";
 import Button from "./button";
 import CategoryToggle from "./category-toggle";
 import CompressionWrapper from "./compression-wrapper";
+import EightProductGrid from "./eight-product-grid";
 import HighlightedText from "./highlighted-text";
 import IconAndText from "./icon-and-text";
 import Input from "./input";
@@ -13,6 +14,7 @@ import SideChatButton from "./side-chat-button";
 import Spacer from "./spacer";
 import TopButton from "./top-button";
 export {
+	EightProductGrid,
 	Banner,
 	Button,
 	CategoryToggle,
