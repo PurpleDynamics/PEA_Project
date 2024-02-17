809a4038c951a371c5ac2efeb413ddaa57175ae6
hayoung78 <wendy4332@naver.com>
2024-02-17 23:56:11 +0900

    chore: docs 수정

--- src/components/used-product/used-trade.jsx
+++ src/components/used-product/used-trade.jsx
@@ -3,48 +3,53 @@ import styled from "styled-components";
 import { COLOR, FONT_SIZE } from "../../libs/styled-components";
 import { HighlightedText, ProductCard } from "../commons";
 
-const UsedTrade = ({ data }) => {
+/**
+ * @component
+ * @parameter usedData : array<중고상품데이터> - used-product페이지안에  usedProductsData 데이터를 받아옵니다.
+ * @returns {JSX.Element}
+ *
+ * @description
+ * - used-product-page 에서 사용되는 컴포넌트입니다.
+ * - 중고거래에 해당하는 productCard를 보여주는 컴포넌트입니다.
+ */
+const UsedTrade = ({ usedData }) => {
 	return (
-		<>
-			{/* <CompressionWrapper> */}
-			<S.Wrapper>
-				<S.LocationTitleContainer>
-					{data && (
-						<S.LocationTitle>
-							{data[0]?.location}{" "}
-							<HighlightedText
-								color={COLOR.PALETTE.mint.weight}
-								fontSize={FONT_SIZE.bg}
-							>
-								중고거래
-							</HighlightedText>
-						</S.LocationTitle>
-					)}
-				</S.LocationTitleContainer>
-				<S.ProductCardWrapper>
-					{data?.map((item, index) => (
-						<ProductCard
-							key={index}
-							productId={index}
-							title={item?.title}
-							imgUrl="https://source.unsplash.com/random/?pigeon"
-							createdAt="2023-03-26"
-							price={item?.price}
-							interestCount={10}
-							chattingCount={20}
-							categoriesArray={["사람", "기타"]}
-						/>
-					))}
-				</S.ProductCardWrapper>
-			</S.Wrapper>
-			{/* </CompressionWrapper> */}
-		</>
+		<S.Wrapper>
+			<S.LocationTitleContainer>
+				{usedData && (
+					<S.LocationTitle>
+						{usedData[0]?.location}{" "}
+						<HighlightedText
+							color={COLOR.PALETTE.orange.weight}
+							fontSize={FONT_SIZE.bg}
+						>
+							중고거래
+						</HighlightedText>
+					</S.LocationTitle>
+				)}
+			</S.LocationTitleContainer>
+			<S.ProductCardWrapper>
+				{usedData?.map((item, index) => (
+					<ProductCard
+						key={index}
+						productId={index}
+						title={item?.title}
+						imgUrl="https://source.unsplash.com/random/?pigeon"
+						createdAt="2023-03-26"
+						price={item?.price}
+						interestCount={10}
+						chattingCount={20}
+						categoriesArray={["사람", "기타"]}
+					/>
+				))}
+			</S.ProductCardWrapper>
+		</S.Wrapper>
 	);
 };
 export default UsedTrade;
 
 const Wrapper = styled.div`
-	width: 100%;
+	width: 115.2rem;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -56,10 +61,10 @@ const LocationTitle = styled.div`
 	text-align: center;
 `;
 const LocationTitleContainer = styled.div`
-	padding: 3.5rem;
+	padding: 3.5rem 0;
 `;
 const ProductCardWrapper = styled.div`
-	width: 115.2rem;
+	width: 100%;
 	display: grid;
 	grid-template-columns: repeat(4, 1fr);
 	row-gap: 2rem;
--- src/pages/used-product-page.jsx
+++ src/pages/used-product-page.jsx
@@ -5,9 +5,15 @@ import { Banner, Spacer } from "../components/commons";
 import { FreeShare, UsedTrade } from "../components/used-product";
 import { COLOR } from "../libs/styled-components";
 
+/**
+ * @component
+ * @returns {JSX.Element}
+ * @description "중고거래" 버튼 혹은 "무료나눔" 버튼 클릭시, 해당 컴포넌트를 불러오는 페이지입니다.
+ */
+
 const UsedProductPage = () => {
 	const [selectedButton, setSelectedButton] = useState("usedTrade");
-
+	//임시 데이터
 	const usedProductsData = [
 		{ title: "최하영", price: "1000000", location: "역삼동" },
 		{ title: "김진솔", price: "1000000", location: "역삼동" },
@@ -31,6 +37,7 @@ const UsedProductPage = () => {
 		{ title: "김진솔무료", price: "00000", location: "역삼동" },
 	];
 
+	// 버튼 클릭시, 버튼색 변경하는 이벤트함수
 	const onClickTypeSelector = (buttonKey) => {
 		const defaultColor = `${COLOR.COMMON[400]}`;
 		if (buttonKey === selectedButton) {
@@ -60,10 +67,10 @@ const UsedProductPage = () => {
 						무료나눔
 					</S.TradeTypeSelector>
 					{selectedButton === "usedTrade" && (
-						<UsedTrade data={usedProductsData} />
+						<UsedTrade usedData={usedProductsData} />
 					)}
 					{selectedButton === "freeShare" && (
-						<FreeShare data={freeProductsData} />
+						<FreeShare freeData={freeProductsData} />
 					)}
 				</S.TradeTypeWrapper>
 			</S.Wrapper>
