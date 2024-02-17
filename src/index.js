a6aa937c20d15ef7ba27bbc2f83b8e756211e82a
yunshin <2younsin@gmail.com>
2024-02-17 18:40:19 +0900

    config: react-query 의존성 추가 및 Provider 설정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
 
 import { OverlayProvider } from "./contexts";
@@ -11,12 +12,22 @@ if (process.env.NODE_ENV === "development") {
 	worker.start();
 }
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		staleTime: 1000 * 60 * 60 * 6, // 유통시간: 6시간
+		cacheTime: 1000 * 60 * 60 * 12, // 페기시간: 12시간
+		refetchOnMount: false, // 페이지 이동 후 복귀 시, refetching 여부
+	},
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<GlobalStyles />
-		<OverlayProvider>
-			<RouterProvider router={router} />
-		</OverlayProvider>
+		<QueryClientProvider client={queryClient}>
+			<GlobalStyles />
+			<OverlayProvider>
+				<RouterProvider router={router} />
+			</OverlayProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
