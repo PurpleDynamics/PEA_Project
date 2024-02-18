684a0eca36776c0ab1fc9f08a9f3cc8d67cff5a4
yunshin <2younsin@gmail.com>
2024-02-18 15:16:23 +0900

    chore: queryClient 에 대한 주석 추가

--- src/index.js
+++ src/index.js
@@ -12,8 +12,13 @@ if (process.env.NODE_ENV === "development") {
 	worker.start();
 }
 
+/**
+ * react-query 로 관리할 모든 요청, 응답과 상호작용하는 객체
+ * 데이터 캐싱 및 접근 등과 관려된 유용한 메서드 제공
+ */
 const queryClient = new QueryClient({
 	defaultOptions: {
+		// 옵션 기본값 지정
 		staleTime: 1000 * 60 * 60 * 6, // 유통시간: 6시간
 		cacheTime: 1000 * 60 * 60 * 12, // 페기시간: 12시간
 		refetchOnMount: false, // 페이지 이동 후 복귀 시, refetching 여부
