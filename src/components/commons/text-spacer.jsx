85633299184ee264413625ba60f7fb2f3f9a39de
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-02-19 01:03:19 +0900

    fix: jsdocs 수정

--- src/components/commons/text-spacer.jsx
+++ src/components/commons/text-spacer.jsx
@@ -5,7 +5,9 @@ import styled from "styled-components";
  * @parameter spacer: number - 숫자를 입력 받은 만큼 space bar 만큼의 공백을 입력합니다.
  * @returns {JSX.Element}
  *
- * @description 글자 사이에 {" "} 혹은 space가 안되는 곳에 사용할 수 있는 text-spacer입니다.
+ * @description
+ * - 글자 사이에 {" "} 혹은 space가 안되는 곳에 사용할 수 있는 text-spacer입니다.
+ * - 기본값은 1 입니다.
  */
 
 const TextSpacer = ({ spacer = 1 }) => {
