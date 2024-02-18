9e0b6c2ab0f58a69bcb75bd45e3f0df267af9619
yunshin <2younsin@gmail.com>
2024-02-18 15:47:04 +0900

    chore: jsDocs 카테고리 이름 수정

--- src/libs/axios/base/products.js
+++ src/libs/axios/base/products.js
@@ -18,7 +18,7 @@ export const getProductsByPaymentMethod = async ({ userId, paymentMethod }) => {
 /**
  * @function
  * @parameter userId : number - 조회하는 사용자의 id
- * @parameter categories : Array<"디지털기기" | "생활가전" | "가전/인테리어" | "생활/주방" | "유아동" | "유아도서" | "여성의류" | "여성잡화" | "식물" | "남성패션/잡화" | "뷰티/미용" | "스포츠/레저" | "취미/게임/음반" | "도서" | "티켓/교환권" | "가공식품" | "반려동물용품" | "기타" > - 카테고리 배열
+ * @parameter categories : Array<"디지털기기" | "생활가전" | "가전/인테리어" | "생활/주방" | "유아동" | "유아도서" | "여성의류" | "여성잡화" | "식물" | "남성패션/잡화" | "뷰티/미용" | "스포츠/레저" | "취미/게임/음반" | "도서" | "티켓/교환권" | "가공식품" | "반려동물용품" | "기타중고물품" > - 카테고리 배열
  * @description 전달된 카테고리 배열에 해당하는 상품 요약 정보를 반환합니다.
  */
 export const getProductsByCategories = async ({
