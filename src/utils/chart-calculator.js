9bedbdda364e55e01c32238e93ed994c2bdc118e
snkchan <144839872+snkchan@users.noreply.github.com>
2024-02-21 00:25:17 +0900

    fix: 잘못된 docs수정

--- src/utils/chart-calculator.js
+++ src/utils/chart-calculator.js
@@ -1,8 +1,7 @@
 /**
  * @function
  * @returns {Array}
- * @description
- * - 현재사용자의 '월'을 포함한 최근6개월의 '월'정보를 받아오는 Array를 return해줍니다.\
+ * @description 현재사용자의 '월'을 포함한 최근6개월의 '월'정보를 받아오는 Array를 return해줍니다.
  */
 export const getRecent6MonthsArray = () => {
 	const thisMonth = new Date().getMonth() + 1; // 현재 '월'에서 -1된 값을 반환해서 +1해준겁니다.
@@ -11,5 +10,5 @@ export const getRecent6MonthsArray = () => {
 		.map((month, idx) => {
 			return ((month - idx) % 12) + 1;
 		})
-		.reverse(); // 배열의 마지막 요소가 현재 '월'이어야 하기때문에 revers()를 해줍니다.
+		.reverse(); // 배열의 마지막 요소가 현재 '월'이어야 하기때문에 reverse()를 해줍니다.
 };
