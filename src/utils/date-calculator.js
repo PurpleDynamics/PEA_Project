d745a1d6aa674f2d6f8a46f4bcffc168bee5c79b
snkchan <144839872+snkchan@users.noreply.github.com>
2024-02-21 02:27:36 +0900

    remove: 날짜 계산을위한 로직 data-calculator.js 로 통합

--- src/utils/date-calculator.js
+++ src/utils/date-calculator.js
@@ -40,3 +40,18 @@ export const printPeriodAsString = ({ formattedDate }) => {
 		return Math.floor(diffDate / 30) + " 달 전"; // 1년 (= 365일) 이상이 아니라면 "월" 단위로 반환
 	else return Math.floor(diffDate / (30 * 12)) + " 년 전"; // 1년 이상이라면 "년" 단위로 반환
 };
+
+/**
+ * @function
+ * @returns {Array}
+ * @description 현재사용자의 '월'을 포함한 최근6개월의 '월'정보를 받아오는 Array를 return해줍니다.
+ */
+export const getRecentNthMonthsArray = ({ Nmonth }) => {
+	const thisMonth = new Date().getMonth() + 1; // 현재 '월'에서 -1된 값을 반환해서 +1해준겁니다.
+	return Array(Nmonth)
+		.fill(thisMonth + 11)
+		.map((month, idx) => {
+			return ((month - idx) % 12) + 1;
+		})
+		.reverse(); // 배열의 마지막 요소가 현재 '월'이어야 하기때문에 reverse()를 해줍니다.
+};
