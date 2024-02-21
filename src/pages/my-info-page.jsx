ac9f355d898bb2e158bc284df843beafc5ecea6f
snkchan <144839872+snkchan@users.noreply.github.com>
2024-02-21 12:12:33 +0900

    docs: my-info docs작성

--- src/pages/my-info-page.jsx
+++ src/pages/my-info-page.jsx
@@ -6,6 +6,13 @@ import {
 	ManageProductsSection,
 	ProductListSection,
 } from "../components/my-info";
+/**
+ * @component
+ * @returns {JSX.Element}
+ * @description
+ * - 사용자의 정보를 보여주는 페이지 컴포넌트 입니다.
+ * - 사용자의 개인정보, 최근거래 금액&chart, 상품관련 정보, 상품을 보여주는 card로 구성되어있습니다.
+ */
 const MyInfoPage = () => {
 	return (
 		<S.MainWrapper>
