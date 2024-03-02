e73662a8358f0b78f6a45353a13d2e3c8ca65e7a
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-03 00:57:15 +0900

    fix: chat token storage 추가

--- src/libs/axios/auth/storage-manager.js
+++ src/libs/axios/auth/storage-manager.js
@@ -2,7 +2,7 @@
  * sessionStorage를 사용하여 애플리케이션 인증 토큰을 관리하는 파일입니다.
  */
 
-import { ACCESS_TOKEN } from "../../../constants";
+import { ACCESS_TOKEN, CHAT_TOKEN } from "../../../constants";
 
 /**
  * @function
@@ -17,6 +17,15 @@ export const setSessionToken = (token) => {
 
 /**
  * @function
+ * @parameter chatToken: string - sessingStorage에 저장될 user안의 socket 값
+ * @returns {void}
+ */
+export const setChattingToken = (chatToken) => {
+	return sessionStorage.setItem(CHAT_TOKEN, chatToken);
+};
+
+/**
+ * @function
  * @returns {string | null} 저장된 token값 또는 token이 없을 경우 null
  *
  * @description sessionStorage에 key값("access_token")으로 받아온 token을 저장하는 함수입니다.
@@ -24,4 +33,15 @@ export const setSessionToken = (token) => {
 
 export const getSessionToken = () => {
 	return sessionStorage.getItem(ACCESS_TOKEN);
+};
+
+/**
+ * @function
+ * @returns {string | null} 저장된 token값 또는 token이 없을 경우 null
+ *
+ * @description sessionStorage에 key값("chattingToken")으로 받아온 token을 저장하는 함수입니다.
+ */
+
+export const getChattingToken = () => {
+	return sessionStorage.getItem(CHAT_TOKEN);
 };
