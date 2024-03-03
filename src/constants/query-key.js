2c6903487bfc850f9c45d8a5f1425b0fedd270b9
yunshin <2younsin@gmail.com>
2024-03-03 14:05:21 +0900

    feat: getProduct(), react-query 활용 query 최적화

--- src/constants/query-key.js
+++ src/constants/query-key.js
@@ -1,4 +1,1 @@
-export const GET_PRODUCT_LIST_BY_PAYMENT_METHOD_KEY =
-	"getProductListByPaymentMethodKey";
-export const GET_PRODUCT_LIST_BY_CATEGORIES_KEY = "getProductListByCategoryKey";
-export const GET_PRODUCT_ONE_KEY = "getProductOneKey";
+export const GET_PRODUCT_LIST = "getProductList";
