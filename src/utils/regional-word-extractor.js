0fe2adda41d6c9266741d56ec514740f5bc2005c
yunshin <2younsin@gmail.com>
2024-03-05 08:58:52 +0900

    chore: JsDocs 컨벤션에 맞도록 수정

--- src/utils/regional-word-extractor.js
+++ src/utils/regional-word-extractor.js
@@ -1,7 +1,7 @@
 /**
  * @function
- * @parameters koreanRegion : string - 한국식 주소
- * @parameters targetRegionalUnit : string - 추출하려는 한국식 행정구역 단위 ("도","시","구","동" 등)
+ * @parameter koreanRegion : string - 한국식 주소
+ * @parameter targetRegionalUnit : string - 추출하려는 한국식 행정구역 단위 ("도","시","구","동" 등)
  * @description
  * - 한국식 행정구역 단위 중 특정 주소만 추출합니다.
  * - 입력받은 주소에서 targetRegionalUnit 에 해당하는 주소가 없을 경우, 최초 입력된 주소를 그대로 반환합니다.
